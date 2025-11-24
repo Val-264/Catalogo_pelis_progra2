b70c5c6a37516d7ec084e22f8682af74694e01f9
Danna103 <al550210@edu.uaa.mx>
2025-11-24 10:55:21 -0600

    cambieusuario

--- Usuario.h
+++ Usuario.h
@@ -1,33 +1,109 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include"peliculas.h"
 using namespace std;
 
-class menuUsuario{
+class Usuarios{
     private: 
-
+        char claveUsuario[12];
+        char titulo[TAM];
+        char director[TAM];
+        char genero[TAM];
+        char sinopsis[TAM*2];
+        int anio;
+        float puntuacion;
     public:
 
-};
+    void menuUsuario(){
+        int opc;
+        do  {
+            cout << " --MENU DE USUARIO--  \n";
+            cout << "1. Ver catalogo de peliculas completo\n";
+            cout << "2. Ver peliculas mejor valoradas\n";
+            cout << "3. Agregar Resena a una pelicula\n";
+            cout << "4. Calificar pelicula\n";
+            cout << "5. Buscar pelicula\n";
+            cout << "6. Salir\n";
+            cout << "   Selecciona una opcion...    \n";
+            cin>>opc;
 
-int main(){
-    int opc;
-
-    do {
-        cout << "--------- MENU DE USUARIO ---------\n";
-        cout << "Elija la opcion que desea: \n";
-        cout << "1- Ver peliculas con mas estrellas \n";
-        cout << "2- Buscar peliculas\n"; 
-        cout << "3- Calificar una pelicula \n";
-        cout << "4- Agregar la sinopsis de una pelicula\n";
-
-        switch (opc) {
-        case 1: 
-            break;
+            switch (opc)
+            {
+            case 1: 
+                break;
+            
+            default:
+                break;
+            }
+        } while (opc!=6);
         
-        default:
-            break;
+    }
+    //Funciones del usuario
+    void verCatalogoCom(){
+        ifstream archivo("catalogo.dat", ios::binary);
+        string linea;
+        Pelicula p;
+
+        if (!archivo.is_open()) {
+            cout << "No se puede abrir el catalogo\n";
+            return;
         }
-    } while (opc !=5);
+        cout << "\n -CATALOGO COMPLETO- \n";
+
+        while (archivo.read((char*)&p, sizeof(Pelicula)))  {
+            cout << "--------------------------------\n";
+            cout << "Titulo: " << p.titulo << "\n";
+            cout << "Director: " << p.director << "\n";
+            cout << "Genero: " << p.genero << "\n";
+            cout << "Anio: " << p.anio << "\n";
+            cout << "Puntuacion: " << p.puntuacion << "\n";
+            cout << "--------------------------------\n";
+        }
+    }
+        archivo.close();
+
     
-}
+    void PelMejoresVal(){
+        ifstream archivo("catalogo.txt");
+        
+
+        if (!archivo.is_open()) {
+            cout << "No se puede abrir el catalogo\n";
+            return;
+        }
+
+    }
+    void buscarPeliculas(){
+        int opc1;
+        ifstream archivo("catalogo.txt");
+        
+
+        if (!archivo.is_open()) {
+            cout << "No se puede abrir el catalogo\n";
+            return;
+        }
+
+
+
+        do{
+            cout << " MENU DE BUSQUEDA \n";
+            cout << " 1. Buscar por genero\n ";
+            cout << " 2. Buscar por anio\n ";
+            cout << " 3. Buscar por titulo\n ";
+            cout << " 4. Salir del menu de busqueda\n ";
+            cout << " Selecciona tu opcion... ";
+            cin >> opc1;
+        } while (opc1 !=3);
+
+        
+    }
+    void setResenas(){
+
+    }
+
+    void calificar();
+
+
+
+};
